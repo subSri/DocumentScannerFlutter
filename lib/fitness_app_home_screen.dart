c123d237d7b232807d4abe6de2940d50be307c14
Ravi Jain <ravijainpro@gmail.com>
2020-07-15 20:14:40 +0530

    Image View UI fixed

--- lib/fitness_app_home_screen.dart
+++ lib/fitness_app_home_screen.dart
@@ -1,12 +1,32 @@
 import 'package:scanbot_sdk_example_flutter/pages_repository.dart';
 import 'package:scanbot_sdk_example_flutter/pdfviewcurved/detailsPage.dart';
-
+import 'dart:convert';
+import 'dart:io';
 import 'fitness_app/models/tabIcon_data.dart';
 import 'fitness_app/traning/training_screen.dart';
 import 'package:flutter/material.dart';
 import 'fitness_app/bottom_navigation_view/bottom_bar_view.dart';
 import 'fitness_app/fintness_app_theme.dart';
 import 'fitness_app/my_diary/my_diary_screen.dart';
+import 'package:scanbot_sdk_example_flutter/ui/preview_document_widget.dart';
+import 'package:scanbot_sdk_example_flutter/ui/progress_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:scanbot_sdk/barcode_scanning_data.dart';
+import 'package:scanbot_sdk/common_data.dart';
+import 'package:scanbot_sdk/document_scan_data.dart';
+import 'package:scanbot_sdk/ehic_scanning_data.dart';
+import 'package:scanbot_sdk/mrz_scanning_data.dart';
+import 'package:scanbot_sdk/scanbot_sdk.dart';
+import 'package:scanbot_sdk/scanbot_sdk_models.dart';
+import 'package:scanbot_sdk/scanbot_sdk_ui.dart';
+import 'package:scanbot_sdk_example_flutter/fitness_app/my_diary/meals_list_view.dart';
+import 'package:scanbot_sdk_example_flutter/fitness_app/traning/training_screen.dart';
+import 'fitness_app_home_screen.dart';
+import 'pages_repository.dart';
+import 'ui/menu_items.dart';
+import 'ui/utils.dart';
+import 'package:flutter/material.dart';
 
 class FitnessAppHomeScreen extends StatefulWidget {
   @override
@@ -71,6 +91,7 @@ class _FitnessAppHomeScreenState extends State<FitnessAppHomeScreen>
     return true;
   }
 
+
   Widget bottomBar() {
     return Column(
       children: <Widget>[
@@ -81,7 +102,7 @@ class _FitnessAppHomeScreenState extends State<FitnessAppHomeScreen>
           tabIconsList: tabIconsList,
           addClick: () {},
           changeIndex: (int index) {
-            if (index == 0 || index == 3) {
+            if (index == 0) {
               animationController.reverse().then<dynamic>((data) {
                 if (!mounted) {
                   return;
@@ -92,7 +113,29 @@ class _FitnessAppHomeScreenState extends State<FitnessAppHomeScreen>
                 });
               });
             }
-            else if (index == 1 || index == 3) {
+            else if (index == 1) {
+              animationController.reverse().then<dynamic>((data) {
+                if (!mounted) {
+                  return;
+                }
+                setState(() {
+                  tabBody =
+                      MyDiaryScreen(animationController: animationController);
+                });
+              });
+            }
+            else if (index == 2) {
+                animationController.reverse().then<dynamic>((data) {
+                if (!mounted) {
+                  return;
+                }
+                setState(() {
+                  tabBody =
+                      MyDiaryScreen(animationController: animationController);
+                });
+              });
+            }
+            else if (index == 3) {
               animationController.reverse().then<dynamic>((data) {
                 if (!mounted) {
                   return;
